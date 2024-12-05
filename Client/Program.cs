1b3211a197ec22d737af1d9dce75841a83a87d93
Pritesh More <priteshpmore@gmail.com>
2024-12-05 20:33:56 +0530

    Dynamic Theme changer added for two property backgroud and primarytext

--- Client/Program.cs
+++ Client/Program.cs
@@ -2,6 +2,7 @@ using Microsoft.AspNetCore.Components.Web;
 using Microsoft.AspNetCore.Components.WebAssembly.Hosting;
 using BlazorApp.Client;
 using MudBlazor.Services;
+using BlazorApp.Client.Utility;
 
 var builder = WebAssemblyHostBuilder.CreateDefault(args);
 builder.RootComponents.Add<App>("#app");
@@ -9,4 +10,5 @@ builder.RootComponents.Add<HeadOutlet>("head::after");
 
 builder.Services.AddScoped(sp => new HttpClient { BaseAddress = new Uri(builder.Configuration["API_Prefix"] ?? builder.HostEnvironment.BaseAddress) });
 builder.Services.AddMudServices();
+builder.Services.AddSingleton<ThemeSetup>();
 await builder.Build().RunAsync();
