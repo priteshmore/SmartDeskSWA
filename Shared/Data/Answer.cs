e04e26198bcc99682293f0141e028e5f478076ca
Pritesh More <priteshpmore@gmail.com>
2025-03-06 17:42:15 +0530

    added question related panel

---
+++ Shared/Data/Answer.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace BlazorApp.Shared.Data
+{
+    public class Answer
+    {
+        public string Value { get; set; }
+        public string Reference { get; set; }
+    }
+}
---
+++ Shared/Data/Question.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace BlazorApp.Shared.Data
+{
+    public class Question
+    {
+        public int Id { get; set; }
+        public string Title { get; set; }
+
+        public Answer AnswerData { get; set; }
+    }
+}
